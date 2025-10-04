fcb418bf94352b38d4387fc959ed9f0fce6e9f27
Katarina Bajnokova <r0997616@student.thomasmore.be>
2025-10-04 10:35:29 +0200

    index and main.js updated

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,20 @@
-
-
-/** @type { import('@storybook/react-vite').StorybookConfig } */
+/** @type {import('@storybook/react-vite').StorybookConfig} */
 const config = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-onboarding",
+    "@storybook/addon-vitest",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
 };
+
 export default config;
